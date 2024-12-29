765fe413b75e09786fad3173a3ae449c2583c6b1
LUZAmaxxp <ayman.allouch@e-polytechnique.ma>
2024-12-29 18:47:25 +0100

    add:animation to the herosection

---
+++ js/AnimationHer.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const heroSection = document.querySelector(".hero");
+  heroSection.classList.add("loaded"); // Add 'loaded' class after DOM is ready
+});
